2a94f62b896fa0d807ac5caee1e6c8acaa342287
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-11-04 19:49:25 -0300

    Criando controller de Animais

--- src/back-end/src/app.js
+++ src/back-end/src/app.js
@@ -1,12 +1,25 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+import animalController from './controllers/Animal.js'
+
+dotenv.config()
 
 const app = express()
 
+app.use(cors())
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.use('/animais', animalController)
+
 app.get('/', (req, res) => {
-    res.send(JSON.stringify({
-        name: 'Samuel',
-        idade: 24
-    }))
+    return res.end('Hello world!')
 })
 
 app.listen(3000, () => console.log('Rodando na porta 3000'))
