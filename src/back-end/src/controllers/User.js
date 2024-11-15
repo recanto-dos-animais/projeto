f31155a10761b1e1707b360d924b2b1bd3025997
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-11-15 19:47:48 -0300

    Rota de criação de usuários e de login

--- src/back-end/src/controllers/User.js
+++ src/back-end/src/controllers/User.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import db from '../../db/db-config.js'
 import { randomUUID } from 'node:crypto'
+import InvalidLogin from '../../errors/InvalidLogin.js'
 import bcrypt from 'bcrypt'
 
 const router = express.Router()
@@ -9,13 +10,35 @@ router.post('/registrar', async (req, res) => {
     const {name, email, password} = req.body
 
     try {
-        const hashedPass = await bcrypt.hash(password, 10)
+        const result = await db(`INSERT INTO usuarios (id, nome, email, senha) VALUES ($1, $2, $3, $4) RETURNING id, nome, email`, [randomUUID(), name, email, await bcrypt.hash(password, 10)])
 
-        const result = await db(`INSERT INTO usuarios (id, nome, email, senha) VALUES ($1, $2, $3, $4) RETURNING id, nome, email`, [randomUUID(), name, email, hashedPass])
-        console.log(result)
         return res.send(JSON.stringify(result.rows))
     } catch (error) {
         console.error(error)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const {email, password} = req.body
+
+    try{
+        const result = await db('SELECT (email, senha) FROM usuarios WHERE email = $1', [email])
+
+        if (result.rows.length > 0) {
+            const user = result.rows[0]
+
+            if (await bcrypt.compare(password, user.senha)) 
+                return res.status(200).json({
+                    authorized: true
+                })
+
+            return res.status(401).json({error: 'Senha inválida'})
+        }
+
+        return res.status(401).json({error: 'Usuário com este email não existe'})
+    }
+    catch(error){
+        return res.status(500).send(JSON.stringify({error: 'Erro interno ao processar login.'}))
     }
 })
 
